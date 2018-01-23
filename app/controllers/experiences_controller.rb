50f45d0b6f0a279eba8d88179300ff42842693fc
Rob Cho <robhcho@gmail.com>
2018-01-22 20:50:29 -0500

    create a migration adding student id to experience and education tables and create fake data using faker

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,6 @@
 class ExperiencesController < ApplicationController
+  def index
+    experiences = Experience.all
+    render json: experiences.as_json
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123010126) do
+ActiveRecord::Schema.define(version: 20180123014550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180123010126) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
   create_table "experiences", force: :cascade do |t|
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180123010126) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
   end
 
 end
