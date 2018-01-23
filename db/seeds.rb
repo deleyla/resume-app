ec65e86ecd01747d7072e598ac15941e3f0dd276
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2018-01-22 20:14:26 -0600

    Create student model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Student.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  phone_number: Faker::PhoneNumber.phone_number,
+  short_bio: Faker::Lorem.paragraph,
+  linkedin_url: Faker::Internet.url('linkedin.com/in/'),
+  twitter_handle: Faker::Twitter.screen_name,
+  personal_blog_url: Faker::Internet.url,
+  online_resume_url: Faker::Internet.url, 
+  github_url: Faker::Internet.user_name,
+  photo: "photo.jpg",
+  password: "password"
+  )
+
