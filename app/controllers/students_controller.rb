734e02fc6356404bf4f6c41dcaf3dbfd10d0852b
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2018-01-22 20:54:10 -0600

    Create routes and controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,28 @@
 class StudentsController < ApplicationController
-  
+  def index
+    students = Student.all
+    render json: students.as_json
+  end
+
+  def create
+    student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_blog_url: params[:personal_blog_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      photo: params[:photo],
+      password_digest: params[:password_digest])
+
+    def show
+      student = Student.find_by(id: current_user.id)
+      render json: student.as_json
 end
+
+
+#current_user.id for show
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+  # experiences: experiences.as_json
+  # educations: educations.as_json
+  # skills: skills.as_json
+  # capstones: capstones.as_json
+
+def as_json
+  
 end
