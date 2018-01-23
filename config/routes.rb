79e84e4a4d266f00ae3885de2bc5aeb5272e7945
Rob Cho <robhcho@gmail.com>
2018-01-22 21:29:07 -0500

    Add CRUD actions to educations and experiences controllers and create each route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get '/experiences' => 'experiences#index'
+  get '/experiences/:id' => 'experiences#show'
+  post '/experiences' => 'experiences#create'
+  patch '/experiences/:id' => 'experiences#update'
+  delete '/experiences/:id' => 'experiences#destroy'
+
+  get '/educations' => 'educations#index'
+  get '/educations/:id' => 'educations#show'
+  post '/educations' => 'educations#create'
+  patch '/educations/:id' => 'educations#update'
+  delete '/educations/:id' => 'educations#destroy'
 end
