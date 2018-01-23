7747a79fc297bf292671ffdd561e28df838a08eb
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2018-01-22 21:17:59 -0600

    Defined the student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,28 @@
 class Student < ApplicationRecord
   has_secure_password
-  
-  # experiences: experiences.as_json
-  # educations: educations.as_json
-  # skills: skills.as_json
-  # capstones: capstones.as_json
+  has_many :experiences
+  has_many :educations
+  has_many :skills
+  has_many :capstones
 
-def as_json
+  def as_json
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone_number: phone_number, 
+      short_bio: short_bio,
+      linkedin_url: linkedin_url,
+      twitter_handle: twitter_handle,
+      personal_blog_url: personal_blog_url, 
+      online_resume_url: online_resume_url, 
+      github_url: github_url, 
+      photo: photo,
+      experiences: experiences.as_json,
+      educations: educations.as_json,
+      skills: skills.as_json,
+      capstones: capstones.as_json
+    }
+  end
 
 end
