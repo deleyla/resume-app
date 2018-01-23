ec010015a373f59fd3849c7eb2a971a9cf90f23b
“deleylagit config --global user.email “deleyla@gmail.com <Deleyla@Davids-MacBook-Pro.local>
2018-01-22 21:04:15 -0600

    Add CRUD to student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,10 +18,38 @@ class StudentsController < ApplicationController
       github_url: params[:github_url],
       photo: params[:photo],
       password_digest: params[:password_digest])
+    student.save
+    render json: student.as_json
+  end
 
-    def show
-      student = Student.find_by(id: current_user.id)
-      render json: student.as_json
+  def show
+    student = Student.find_by(id: current_user.id)
+    render json: student.as_json
+  end
+
+  def update
+    student = Student.find_by(id: current_user.id)
+      first_name = params[:first_name],
+      last_name = params[:last_name],
+      email = params[:email],
+      phone_number = params[:phone_number],
+      short_bio = params[:short_bio],
+      linkedin_url = params[:linkedin_url],
+      twitter_handle = params[:twitter_handle],
+      personal_blog_url = params[:personal_blog_url],
+      online_resume_url = params[:online_resume_url],
+      github_url = params[:github_url],
+      photo = params[:photo],
+      password_digest = params[:password_digest]
+    student.save
+    render json: student.as_json
+  end
+
+  def destroy
+    student = Student.find_by(id: current_user.id)
+    student.destroy
+    render json: {message: "Student successfully destroyed"}
+  end
 end
 
 
