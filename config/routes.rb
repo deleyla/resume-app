2907194500cd6b7ec6d0b2d35c887ec9aea297c1
Rob Cho <robhcho@gmail.com>
2018-01-23 20:13:16 -0500

    Change show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get '/experiences' => 'experiences#index'
-  get '/experiences/:id' => 'experiences#show'
+  get '/experiences/show' => 'experiences#show'
   post '/experiences' => 'experiences#create'
   patch '/experiences/:id' => 'experiences#update'
   delete '/experiences/:id' => 'experiences#destroy'
 
   get '/educations' => 'educations#index'
-  get '/educations/:id' => 'educations#show'
+  get '/educations/show' => 'educations#show'
   post '/educations' => 'educations#create'
   patch '/educations/:id' => 'educations#update'
   delete '/educations/:id' => 'educations#destroy'
